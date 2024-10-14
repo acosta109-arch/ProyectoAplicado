f98f798834d2c187b6873aa4d13b107907bad3f7
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-10-14 11:24:53 -0400

    El Contexto

--- ProyectoAplicado/DAL/Contexto.cs
+++ ProyectoAplicado/DAL/Contexto.cs
@@ -7,6 +7,7 @@ public class Contexto:DbContext
 {
     public Contexto(DbContextOptions<Contexto>options)
     :base(options){ }   
+
     public DbSet<Bebidas> Bebidas { get; set; }
     public DbSet<Cocineros> Cocineros { get; set; }
     public DbSet<Comidas> Comidas { get; set; }
