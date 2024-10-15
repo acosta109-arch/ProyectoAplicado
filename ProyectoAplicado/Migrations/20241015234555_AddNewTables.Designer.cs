a7ce97108155e9306827ecf0dde8f5ffda93088e
Andy Javier <andy_javier@ucne.edu.do>
2024-10-15 19:48:43 -0400

    db y migrations

--- ProyectoAplicado/Migrations/20241015234555_AddNewTables.Designer.cs
+++ ProyectoAplicado/Migrations/20241015234555_AddNewTables.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProyectoAplicado.DAL;
 
@@ -10,9 +11,11 @@ using ProyectoAplicado.DAL;
 namespace ProyectoAplicado.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20241015234555_AddNewTables")]
+    partial class AddNewTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
