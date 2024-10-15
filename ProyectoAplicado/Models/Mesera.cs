ac671e88b356443bf3b8d1f7ceb026b7583d152f
Andy Javier <andy_javier@ucne.edu.do>
2024-10-15 19:39:13 -0400

    Cree el modelo de Mesera

---
+++ ProyectoAplicado/Models/Mesera.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+namespace ProyectoAplicado.Models;
+public class Mesera
+{
+    [Key]
+    public int MeseraId { get; set; }
+
+    [Required(ErrorMessage = "El nombre de la mesera es obligatorio.")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El nombre solo puede contener letras.")]
+    public string? Nombre { get; set; }
+
+    [Required(ErrorMessage = "El apellido de la mesera es obligatorio.")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El apellido solo puede contener letras.")]
+    public string? Apellido { get; set; }
+
+    [Required(ErrorMessage = "El teléfono de la mesera es obligatorio.")]
+    [RegularExpression(@"^\d{10}$", ErrorMessage = "El teléfono debe tener 10 dígitos.")]
+    public string? Telefono { get; set; }
+
+    public List<Mesa> Mesas { get; set; } = new List<Mesa>();
+    public List<Comanda> Comandas { get; set; } = new List<Comanda>();
+}
